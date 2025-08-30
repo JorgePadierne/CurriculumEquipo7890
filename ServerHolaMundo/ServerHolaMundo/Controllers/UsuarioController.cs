8f3d984b1177f4d43cc940f231716d186c7e0632
Nestor Triana <nestortriana287@gmail.com>
2025-08-30 13:09:47 -0400

    hola

---
+++ ServerHolaMundo/ServerHolaMundo/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerHolaMundo.Controllers
+{
+    [ApiController]
+  
+    public class UsuarioController : ControllerBase
+    {
+        private readonly Models.UsuariosLogContext _context;
+        public UsuarioController(Models.UsuariosLogContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("/agregar")]
+        public IActionResult CrearUsuario([FromBody] Models.Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+            return Ok(usuario);
+        }
+
+        [HttpGet]
+        [Route("Buscar")]
+        public IActionResult Buscarusuario([FromQuery] string user)
+        {
+            var usuario = _context.Usuarios.FirstOrDefault(_context => _context.User == user);
+            if (usuario == null)
+            {
+                return NotFound("Usuario no encontrado");
+            }
+            return Ok(usuario);
+        }
+        [HttpGet("Buscar@")]
+        public IActionResult BuscarEmail([FromQuery] string email)
+        {
+            var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == email);
+                if (email == null)
+            {
+                return NotFound("Correo electronico no encontrado");
+            }
+            return Ok(usuario);
+        }
+            
+    }
+}
---
+++ ServerHolaMundo/ServerHolaMundo/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServerHolaMundo.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string User { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? Email { get; set; }
+}
