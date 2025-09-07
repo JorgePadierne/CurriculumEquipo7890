8e97bb057db61f341c98b6dbe04ab36bcbc6299b
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-07 19:28:30 +0100

    s

---
+++ ClientHolaMundo/src/Components/Usuarios.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Usuarios() {
+  const [textoInput, setTextoInput] = useState("");
+  const [usuarios, setUsuarios] = useState("");
+
+  const MiAxios = axios.create({
+    baseURL: "http://localhost:5150",
+    timeout: 10000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const fetchData = async (textoInput) => {
+    try {
+      const response = await MiAxios.get(
+        `/api/usuario/buscar?user=${textoInput}`
+      );
+      setUsuarios(response.data);
+    } catch (error) {
+      console.error(
+        "Error en la petición:",
+        error.response?.data || error.message
+      );
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setTextoInput(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    fetchData(textoInput);
+  };
+
+  return (
+    <section className="usuarios">
+      <h2>Buscador de usuarios</h2>
+      <input
+        type="text"
+        value={textoInput}
+        onChange={handleInputChange}
+        placeholder="Escribe algo"
+      />
+      <button onClick={handleButtonClick}>Buscar</button>
+
+      <span>{usuarios}</span>
+    </section>
+  );
+}
+export default Usuarios;
