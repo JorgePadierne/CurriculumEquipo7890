edc090e5d68a4dd8e565b0dc376a336fa647d039
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-12 00:07:25 +0100

    plis

--- Lista_Tareas/Lista_Tareas/Program.cs
+++ Lista_Tareas/Lista_Tareas/Program.cs
@@ -4,53 +4,24 @@ using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Intentar obtener la cadena de conexión completa primero
-var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? 
-                      Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+// Construir la cadena de conexión desde variables individuales
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var port = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var database = Environment.GetEnvironmentVariable("DB_NAME");
+var sslmode = Environment.GetEnvironmentVariable("DB_SSLMODE") ?? "Require";
 
-// Si no hay cadena completa, construir desde variables individuales
-if (string.IsNullOrEmpty(connectionString))
+var connectionString = new NpgsqlConnectionStringBuilder
 {
-    var host = Environment.GetEnvironmentVariable("DB_HOST");
-    var port = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
-    var user = Environment.GetEnvironmentVariable("DB_USER");
-    var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
-    var database = Environment.GetEnvironmentVariable("DB_NAME");
-    var sslmode = Environment.GetEnvironmentVariable("DB_SSLMODE") ?? "Require";
-
-    connectionString = new NpgsqlConnectionStringBuilder
-    {
-        Host = host,
-        Port = int.Parse(port),
-        Username = user,
-        Password = password,
-        Database = database,
-        SslMode = Enum.Parse<SslMode>(sslmode),
-        TrustServerCertificate = true
-    }.ConnectionString;
-}
-else
-{
-    // Si la cadena viene en formato postgres://, convertirla al formato correcto
-    if (connectionString.StartsWith("postgres://"))
-    {
-        connectionString = connectionString.Replace("postgres://", "");
-        
-        // Parsear la cadena de conexión postgres://
-        var uri = new Uri($"postgres://{connectionString}");
-        var builder = new NpgsqlConnectionStringBuilder
-        {
-            Host = uri.Host,
-            Port = uri.Port,
-            Username = uri.UserInfo.Split(':')[0],
-            Password = uri.UserInfo.Split(':')[1],
-            Database = uri.AbsolutePath.TrimStart('/'),
-            SslMode = SslMode.Require,
-            TrustServerCertificate = true
-        };
-        connectionString = builder.ConnectionString;
-    }
-}
+    Host = host,
+    Port = int.Parse(port),
+    Username = user,
+    Password = password,
+    Database = database,
+    SslMode = Enum.Parse<SslMode>(sslmode),
+    TrustServerCertificate = true
+}.ConnectionString;
 
 builder.Services.AddDbContext<ToDoListContext>(options =>
     options.UseNpgsql(connectionString));
