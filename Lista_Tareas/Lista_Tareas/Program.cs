7e0654565d5ab9e09e7ed75f73255353304f0324
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-11 23:44:26 +0100

    render

--- Lista_Tareas/Lista_Tareas/Program.cs
+++ Lista_Tareas/Lista_Tareas/Program.cs
@@ -4,34 +4,23 @@ using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string? databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-string connectionString;
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var port = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var database = Environment.GetEnvironmentVariable("DB_NAME");
+var sslmode = Environment.GetEnvironmentVariable("DB_SSLMODE") ?? "Require";
 
-if (!string.IsNullOrEmpty(databaseUrl))
+var connectionString = new NpgsqlConnectionStringBuilder
 {
-    // Asegúrate que el prefijo sea 'postgres://'
-    if (databaseUrl.StartsWith("postgresql://"))
-        databaseUrl = "postgres://" + databaseUrl.Substring("postgresql://".Length);
-
-    // Parsear la URL
-    var uri = new Uri(databaseUrl);
-    var userInfo = uri.UserInfo.Split(':');
-
-    connectionString = new NpgsqlConnectionStringBuilder
-    {
-        Host = uri.Host,
-        Port = uri.Port > 0 ? uri.Port : 5432,
-        Username = userInfo[0],
-        Password = userInfo.Length > 1 ? userInfo[1] : "",
-        Database = uri.AbsolutePath.TrimStart('/'),
-        SslMode = SslMode.Require,
-        TrustServerCertificate = true
-    }.ConnectionString;
-}
-else
-{
-    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
+    Host = host,
+    Port = int.Parse(port),
+    Username = user,
+    Password = password,
+    Database = database,
+    SslMode = Enum.Parse<SslMode>(sslmode),
+    TrustServerCertificate = true
+}.ConnectionString;
 
 builder.Services.AddDbContext<ToDoListContext>(options =>
     options.UseNpgsql(connectionString));
@@ -61,6 +50,7 @@ app.UseCors("PermitirReact");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
 
 
 
