38d6bb1282f7e3763a6a0dc0d7921dab8560fcc3
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-12 00:40:24 +0100

    ahora si

--- Lista_Tareas/Lista_Tareas/Context/ToDoListContext.cs
+++ Lista_Tareas/Lista_Tareas/Context/ToDoListContext.cs
@@ -21,8 +21,10 @@ public partial class ToDoListContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=ToDoList;Username=postgres;Password=apc5tdrd");
+    {
+        // La configuración de conexión se maneja en Program.cs
+        // No configurar aquí para evitar conflictos
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
